47cad0d6b9a97caf66cf0b7fa06a2598af2f4d00
gina-christians <christiansgina@gmail.com>
2019-07-05 14:13:08 -0600

    build out code for index.js and server.js/api dir

---
+++ api/server.js
@@ -0,0 +1,25 @@
+//import dependencies here
+const express=require('express');
+const helmet=require('helmet');
+const morgan=require('morgan');
+const cors=require('cors');
+
+
+//create server
+const server=express();
+
+//tell server how to parse json
+server.use(express.json());
+//tell server to use dependencies
+server.use(helmet());
+server.use(morgan('combined'));
+server.use(cors());
+
+//server sanity check
+server.get('/', (req,res) => {
+    res.json("Interesting. You\'re afraid of insects and women. Ladybugs must render you catatonic.")
+});
+
+
+//don't forget to export module here
+module.exports=server;
