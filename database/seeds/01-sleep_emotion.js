c888739882ed52e7ea3aeb15aab61707b60a6aaf
gina-christians <christiansgina@gmail.com>
2019-07-06 13:40:45 -0600

    add sleep_emotion seeds

--- database/seeds/01-sleep_emotion.js
+++ database/seeds/01-sleep_emotion.js
@@ -1,13 +1,20 @@
 
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed=function(knex,Promise) {
+  return knex('emotion').insert([
+    {
+      "emotion": "1 - Uber Unhappy"
+    },
+    {
+      "emotion": "2 - Unhappy"
+    },  
+    {
+      "emotion": "3 - Neutral/Happy"
+    },
+    {
+      "emotion": "4 - Uber Happy"
+    },
+    {
+      "emotion": "5 - Sleep stats not logged"
+    }
+  ]);
 };
