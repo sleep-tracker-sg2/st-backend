e9f8c1d428be574b8e18d11d75a14134eeeb6af0
gina-christians <christiansgina@gmail.com>
2019-07-06 15:48:19 -0600

    code out migrations

--- database/migrations/20190706131229_sleep_emotion.js
+++ database/migrations/20190706131229_sleep_emotion.js
@@ -1,11 +1,11 @@
 
 exports.up = function(knex,Promise) {
-  return knex.schema.createTable('emotion', tbl=>{
+  return knex.schema.createTable('sleep_emotion', tbl=>{
       tbl.increments();
       tbl.string('emotion', 255).notNullable();
   });
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists('emotion');
+  return knex.schema.dropTableIfExists('sleep_emotion');
 };
--- database/migrations/20190706131809_sleep_years.js
+++ database/migrations/20190706131809_sleep_years.js
@@ -1,8 +1,11 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('sleep_years', tbl => {
+        tbl.increments();
+        tbl.string('year', 255).notNullable();
+    });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('sleep_years');
 };
