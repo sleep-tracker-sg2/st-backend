207d4d1d0866ca4e782fd35027a2b2f97b4586af
gina-christians <christiansgina@gmail.com>
2019-07-06 14:49:09 -0600

    code out migration emotion, days

--- database/migrations/20190706131229_sleep_emotion.js
+++ database/migrations/20190706131229_sleep_emotion.js
@@ -1,8 +1,11 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex,Promise) {
+  return knex.schema.createTable('emotion', tbl=>{
+      tbl.increments();
+      tbl.string('emotion', 255).notNullable();
+  });
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('emotion');
 };
--- database/migrations/20190706131644_sleep_days.js
+++ database/migrations/20190706131644_sleep_days.js
@@ -1,8 +1,11 @@
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('day', tbl => {
+        tbl.increments();
+        tbl.string('day', 255).notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('day');
 };
